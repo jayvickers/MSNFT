f3978e9f838baf256821c1e44d1fa604b42a33d2
unknown <jacob.e.vickers@gmail.com>
2021-08-04 08:47:02 -0600

    title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import "../styles/global.less"
 const IndexPage = () => {
   return (
     <>
-    <Helmet title="Jay Vickers, Software Engineer">
-    <meta name="description" content="Jay Vickers resume" />
+    <Helmet title="MeanStreak NFT Marketplace & Community">
+    <meta name="description" content="MeanStreak NFT" />
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet"/>
