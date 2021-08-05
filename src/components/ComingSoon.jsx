6e03454f6123624b07a05abe80b43249cdeda54d
unknown <jacob.e.vickers@gmail.com>
2021-08-05 16:23:31 -0600

    style up

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -17,20 +17,23 @@ const ComingSoon = () => {
                 opacity: ".85",
                 backgroundColor: "black"
             }} />
-            <img style={{
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                zIndex: "-1"
-            }} src={bgimg} />
+            <img className="bgimg" src={bgimg} />
             <div className="coming-soon">
                 <div className="coming-soon__text-container">
                     <h1>MeanStreak NFT</h1>
                     <h2>Coming soon 2021</h2>
-                    <p>NFT marketplace and art community. Featuring New England graffiti legends, digital commodies backed by real <span style={underlineStyle}>physical items</span>.</p>
-                    <p>find us on these social platforms for exclusive community drops. custom 1/1 apparel, live events, music, and more.</p>
+                    <p>NFT marketplace and art community - Featuring New England graffiti legends</p>
+                    <p>Digital commodies backed by real <span style={underlineStyle}>physical items</span>.</p>
+                    {/* <p>Featuring New England graffiti legends. digital commodies backed by real <span style={underlineStyle}>physical items</span>.</p> */}
+                    <p>Exclusive community drops, custom 1/1 apparel, live events, music, and more.</p>
+                    <div className="social-container" style={{ display: "flex", justifyContent: "center" }}>
+                        <span className="social-container__find-us">Find us here<i className="fas fa-arrow-right"></i></span>
+                        <span className="social-container__icon-container"> <i className="fab fa-twitter"></i>
+                            <i className="fab fa-discord"></i><i className="fab fa-reddit"></i>
+                        </span>
+                    </div>
                 </div>
-                <div className="coming-soon__gallery">
+                {/* <div className="coming-soon__gallery">
                     <div className="gallery__row-container">
                         <div className="gallery__item"> my gallery item</div>
                         <div className="gallery__item"> my gallery item</div>
@@ -39,7 +42,7 @@ const ComingSoon = () => {
                         <div className="gallery__item"> my gallery item</div>
                         <div className="gallery__item"> my gallery item</div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     );
