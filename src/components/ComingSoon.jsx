2ff312cb9c03d678f1df155f2b340014dd148b54
unknown <jacob.e.vickers@gmail.com>
2021-08-06 08:46:03 -0600

    add logo

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './_coming-soon.less';
-import bgimg from '../images/bg5mono50.webp'
-import bgimg2 from '../images/bg5mono50.jpg'
+import bgimgwebp from '../images/bg5mono50.webp'
+import bgimgjpg from '../images/bg5mono50.jpg'
+import paintdot from '../images/icons/paintdot.png';
+import logopng from '../images/logos/logocrop.png';
+import logowebp from '../images/logos/logocrop.webp';
 
 const ComingSoon = () => {
 
     const underlineStyle = {
         paddingBottom: "3px",
+        marginLeft: "8px",
         boxShadow: "rgb(241 80 47 / 0%) 0px 10px 0px 0px, rgb(241 80 47) 0px -0.125em inset"
     }
     return (
@@ -19,22 +23,43 @@ const ComingSoon = () => {
                 backgroundColor: "black"
             }} />
 
+
+
+
             <picture>
-                <source srcSet={bgimg} type="image/webp" />
-                <source srcSet={bgimg2} type="image/jpeg" />
-                <img className="bgimg" src={bgimg} alt="background graffiti" />
+                <source srcSet={bgimgwebp} type="image/webp" />
+                <source srcSet={bgimgjpg} type="image/jpeg" />
+                <img className="bgimg" src={bgimgwebp} alt="background graffiti" />
             </picture>
             <div className="coming-soon">
+                <picture>
+                    <source srcSet={logowebp} type="image/webp" />
+                    <source srcSet={logopng} type="image/jpeg" />
+                    <img className="coming-soon__logo" src={logowebp} alt="msnft logo" />
+                </picture>
+
                 <div className="coming-soon__text-container">
                     <h1>MeanStreak NFT</h1>
                     <h2>Coming soon 2021</h2>
-                    <p>NFT marketplace and art community - Featuring New England graffiti legends</p>
-                    <p>Digital commodies backed by real <span style={underlineStyle}>physical items</span>.</p>
-                    <p>Exclusive community drops, custom 1/1 apparel, live events, music, and more.</p>
+                    <p className="text-container__bullet">
+                        <img className="text-container__paint-dot" alt="paint dot icon" src={paintdot} />
+                        NFT marketplace and art community - Featuring New England graffiti legends.
+                    </p>
+                    <p className="text-container__bullet">
+                        <img className="text-container__paint-dot" alt="paint dot icon" src={paintdot} />
+                        Digital commodies backed by real
+                        <span style={underlineStyle}>physical items</span>.
+                    </p>
+                    <p className="text-container__bullet">
+                        <img className="text-container__paint-dot" alt="paint dot icon" src={paintdot} />
+                        Exclusive community drops, custom 1/1 apparel, live events, music, and more.</p>
                     <div className="social-container" style={{ display: "flex", justifyContent: "center" }}>
-                        <span className="social-container__find-us">Find us here<i className="fas fa-arrow-right"></i></span>
-                        <span className="social-container__icon-container"> <i className="fab fa-twitter"></i>
-                            <i className="fab fa-discord"></i><i className="fab fa-reddit"></i>
+                        <span className="social-container__find-us">Find us here
+                            <i className="fas fa-arrow-right" /></span>
+                        <span className="social-container__icon-container">
+                            <a href="https://twitter.com/MeanStreakNFT" rel="noreferrer" target="_blank"><i className="fab fa-twitter" /></a>
+                            {/* <i className="fab fa-discord"></i>
+                            <i className="fab fa-reddit"></i> */}
                         </span>
                     </div>
                 </div>
