33378149be313fa178fad78404062b4ab4e3e07b
unknown <jacob.e.vickers@gmail.com>
2021-08-12 05:50:46 -0600

    icon support

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,19 @@ module.exports = {
         name: "images",
         path: "./src/images/",
       },
-      __key: "images",
+      __key: "images", 
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MeanStreakNFT`,
+        short_name: `MSNFT`,
+       // start_url: `/`,
+     //   background_color: `#f7f0eb`,
+     //   theme_color: `#a2466c`,
+        display: `standalone`,
+     //   icon: `src/images/logos/icon.png`,
+      },
     },
   ],
 };
